46e2076e773196d80b10c7321df5665f209a65db
Romanief <federicoromaniello@outlook.com>
2024-05-24 13:13:26 +0100

    Fixed spacing

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import SlidingText from './SlidingText'
 
 function Title() {
   return (
-    <div>
+    <div className='h-[80vh] relative'>
       <SlidingText
       text="Hey,"
       offsetX={-60}
