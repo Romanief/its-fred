a528f8871b021155bf099abce9ca5316abf64fbe
Romanief <federicoromaniello@outlook.com>
2024-05-28 17:19:49 +0100

    Fixed sizing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-screen h-10 bg-darkDimGray border-y border-dimGray pb-10 p-3 text-xl sm:text-3xl flex'>
-      <div className='w-1/2 text-center'>
+    <div className='w-screen h-10 bg-darkDimGray border-y border-dimGray pb-10 p-3 sm:text-3xl text-md flex'>
+      <div className='w-2/3 text-center'>
         Made in <span className='bg-inherit text-saffron'>italy</span>
         , Based in <span className='bg-inherit text-saffron'>london</span>
       </div>
 
-      <div className="flex justify-center lg:ml-20 gap-10 h-10 pb-2 w-1/2">
+      <div className="flex justify-center lg:ml-20 mr-10 gap-10 h-10 pb-2 w-1/3">
         <img className='aspect-video h-full' src='/LSBU.jpg'></img>
         <img className='aspect-video h-full' src='nhs.png'></img>
       </div>
