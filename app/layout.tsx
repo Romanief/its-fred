09f0518b22fe5f1785f35d2f7fd61e18550ee001
Romanief <federicoromaniello@outlook.com>
2024-05-28 15:56:55 +0100

    Fixed favIco

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/f. ico" />
+        <link rel="icon" href="/f.ico" />
       </head>
       <body>
         {children}
