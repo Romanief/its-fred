d4d758be646632e5b684b217bb62c991e90a1b8d
Romanief <federicoromaniello@outlook.com>
2024-05-28 13:02:32 +0100

    Changed spacing

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24 bg-transparent overflow-x-hidden">
       <Title/>
       <Paragraph text={description}/>
-      <Project text={ndDescription} title="Number Recognition" image="/number.jpeg" mt={200} git="https://github.com/Romanief/number-recognition"/>
+      <Project text={ndDescription} title="Number Recognition" image="/number.jpeg" mt={100} git="https://github.com/Romanief/number-recognition"/>
       <Project text={shDescription} title="e-shop" image="/shop.jpeg" mt={70} git="https://github.com/Romanief/shopapp/tree/main"/>
       <Project text={tttDescription} title="Tic Tac Toe" image="/tictactoe.jpeg" mt={70} link="https://romanief.github.io/tictactoe/" git="https://github.com/Romanief/tictactoe"/>
       <div className="h-[14vh]"></div>
