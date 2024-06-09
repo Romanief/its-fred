3d86b2ebac36321355b26dd09c672c54abf2a728
Romanief <federicoromaniello@outlook.com>
2024-06-09 18:17:14 +0100

    Changed order of elements

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
       <Funfact title="Fun fact!" texts={[ff1]}/>
       <Project text={tttDescription} title="Tic Tac Toe" image="/tictactoe.jpeg" mt={10} link="https://romanief.github.io/tictactoe/" git="https://github.com/Romanief/tictactoe"/>
       <Project text={edcDescription} title="EDCaseload" image="/edcaseload.jpeg" git="https://github.com/Romanief/edcaseload"/>
+      <Funfact title="Useless fact!" texts={[ff2]}/>
       <Project text={shDescription} title="e-shop" image="/shop.jpeg" mt={50} git="https://github.com/Romanief/shopapp/tree/main"/>
       {/* <Paragraph text={aboutSh}/>
       <Paragraph text={aboutBe}/> */}
-      <Funfact title="Useless fact!" texts={[ff2]}/>
       {/* <Paragraph text={theEnd}/>
       <Paragraph text={theEndAgain}/> */}
     </main>
